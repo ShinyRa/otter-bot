283f729bc567a5dee62cc4d7b00f3c846cd12c66
Auke Steenman <Auke.steenman@gmail.com>
2020-07-29 21:46:53 +0200

    Made basic bot responses, added the request module. Deprecated?

--- index.js
+++ index.js
@@ -2,17 +2,58 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const env = require("dotenv").config();
 
+const request = require('request');
+
 const { API_KEY } = env.parsed;
 
 bot.on("ready", () => {
   console.log(`Logged in as ${bot.user.tag}!`);
-  bot.channels.cache.get("738040514112258049").send("Otter");
+  //bot.channels.cache.get("738031504940335194").send("YOOOOOOOOOOOOOOOOOOOOOOOOOOO VIS");
 });
 
+
+const PREFIX = "?";
 bot.on("message", (msg) => {
-  if (msg.content === "otter") {
-    msg.reply("otter");
+  if (!msg.content.startsWith(PREFIX)) return;
+
+  switch(msg.content){
+    case "?help":
+      msg.author.send("Hier komt wat Otter wijsheid!\n\n"+
+                      "**?help**: Geeft informatie over het gebruik van de Otter-bot! (op het moment alleen de commands)\n"+
+                      "**?otterdag**: Ehhhhh wanneer is het otterdag dan?!\n"+
+                      "**?rareotter**: Geeft een otter foto gegenereerd door DeepAi\n"+
+                      "**?hoeveelotterdagen**: Hoeveel otterdagen zijn er al geweest sinds de launch van Otter-bot?\n"+
+                      "**?whodis**: Geeft wat info over de gezellige codeurs!\n"+
+                      "**?otter**: Geeft een mooie otter foto!\n\n"+
+                      "waar wacht je nog op! ga praten met de otter in het otter kanaal!"
+                      );
+    break;
+    
+    case "?otterdag":
+      msg.reply("Hoe moet ik dat weten ik ben een otter lol.");
+    break;
+
+    case "?rareotter":
+      msg.reply("ja later oke deze otter is niet zo snel");
+    break;
+
+    case "?hoeveelotterdagen":
+      msg.reply("ehhhhh ongeveer twee uur ofzo.");
+    break;
+
+    case "?whodis":
+      msg.reply("Ik ben tot leven gewekt door Tijs en Auke!");
+    break;
+
+    case "?otter":
+      msg.reply("http://www.cutestpaw.com/wp-content/uploads/2016/01/Tiny-otter..jpg");
+    break;
   }
 });
+
+//This gets a fresh new otter pic from cutestpaw.com
+function getOtterPic(){
+  
+}
 
 bot.login(API_KEY);
