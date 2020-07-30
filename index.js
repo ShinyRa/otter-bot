34e3e6a31f2c2d4153729f6c9feff593448dad25
Auke Steenman <Auke.steenman@gmail.com>
2020-07-30 23:46:26 +0200

    Added a status to the bot. This will show if it is otter day or not

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var isOtterday = false;
 
 bot.on("ready", () => {
   console.log(`Logged in as ${bot.user.tag}!`);
+  bot.user.setActivity("Het is geen otterdag...");
 });
 
 const PREFIX = "?";
@@ -21,52 +22,65 @@ bot.on("message", async (msg) => {
 
   switch (msg.content) {
     case "?help":
-      msg.author.send(
-          "Hier komt wat Otter wijsheid!\n\n" +
-          "**?help**: Geeft informatie over het gebruik van de Otter-bot! (op het moment alleen de commands)\n" +
-          "**?otterdag**: Ehhhhh wanneer is het otterdag dan?!\n" +
-          "**?rareotter**: Geeft een otter foto gegenereerd door DeepAi\n" +
-          "**?hoeveelotterdagen**: Hoeveel otterdagen zijn er al geweest sinds de launch van Otter-bot?\n" +
-          "**?whodis**: Geeft wat info over de gezellige codeurs!\n" +
-          "**?otter**: Geeft een mooie otter foto!\n\n" +
-          "Waar wacht je nog op! ga praten met de otter in het otter kanaal!"
-      );
+        msg.author.send(
+            "Hier komt wat Otter wijsheid!\n\n" +
+            "**?help**: Geeft informatie over het gebruik van de Otter-bot! (op het moment alleen de commands)\n" +
+            "**?otterdag**: Ehhhhh wanneer is het otterdag dan?!\n" +
+            "**?rareotter**: Geeft een otter foto gegenereerd door DeepAi\n" +
+            "**?hoeveelotterdagen**: Hoeveel otterdagen zijn er al geweest sinds de launch van Otter-bot?\n" +
+            "**?whodis**: Geeft wat info over de gezellige codeurs!\n" +
+            "**?otter**: Geeft een mooie otter foto!\n\n" +
+            "Waar wacht je nog op! ga praten met de otter in het otter kanaal!"
+        );
       break;
 
     case "?otterdag":
-      msg.reply(TTOD());
+        msg.reply(TTOD());
       break;
 
     case "?rareotter":
-      try {
-        const url = await getDeepAiOtter();
-        msg.reply("Wow deze heb ik nog nooit gezien!", { files: [url] });
-      } catch (error) {
-        console.log(error);
-      }
+        try {
+          const url = await getDeepAiOtter();
+          msg.reply("Wow deze heb ik nog nooit gezien!", { files: [url] });
+        } catch (error) {
+          console.log(error);
+        }
       break;
 
     case "?hoeveelotterdagen":
-      msg.reply("Ehhhhh ongeveer twee uur ofzo.");
+        msg.reply("Ehhhhh ongeveer twee uur ofzo.");
       break;
 
     case "?whodis":
-      msg.reply("Ik ben tot leven gewekt door Tijs en Auke!");
+        msg.reply("Ik ben tot leven gewekt door Tijs en Auke!");
       break;
 
     case "?otter":
-      msg.reply("Hier een mooie otter pic!", { files: [getOtterPic()] });
+        msg.reply("Hier een mooie otter pic!", { files: [getOtterPic()] });
+      break;
+
+    case "?otter":
+        msg.reply("Hier een mooie otter pic!", { files: [getOtterPic()] });
       break;
   }
 });
 
 //To be or not to be otter day. That is the question.
-schedule.scheduleJob('0 0 * * *', () => { 
+schedule.scheduleJob('30 * * * * *', () => { 
+  //for read-ability I have split the assignment of the isOtterday variable and the bound actions into 2 expressions.
   isOtterday ? isOtterday = false : isOtterday = true;
-  isOtterday ? bot.channels.cache.get("738031504940335194").send("Het is Otter dag!") 
-             : bot.channels.cache.get("738031504940335194").send("Otter dag is afgelopen...");
-      
+  if(isOtterday){
+    bot.user.setActivity("Het is otterdag!", {
+      type: "STREAMING",
+      url: "https://www.youtube.com/watch?v=OjqyQr1Fa4g"
+    });
+    bot.channels.cache.get("738031504940335194").send("@everyone Het is Otter dag!") 
+  }else{
+    bot.user.setActivity("Het is geen otterdag...");
+    bot.channels.cache.get("738031504940335194").send("@everyone Otter dag is afgelopen...");
+  }
 })
+
 //This gets a fresh new otter pic from cutestpaw.com
 function getOtterPic() {
   return "http://www.cutestpaw.com/wp-content/uploads/2016/01/Tiny-otter..jpg";
@@ -74,6 +88,7 @@ function getOtterPic() {
 
 //Time Till Otter Day
 function TTOD(){
+//!!NOTE!! When it reaches a new hour it displays for example 16 hours and 60 minutes. Should be 17 hours.
   if(isOtterday){
     return "Het is al Otter dag!"
   }else{
