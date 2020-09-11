853d0ac476b32153ed6c97e665a271f5806298f9
Tijs de Graaff <tijs@feka.nl>
2020-09-11 16:41:12 +0200

    FIxed testing in build script

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,13 @@ FROM node:14-stretch
 WORKDIR /app
 
 # Copy everything except files in dockerignore
-COPY . ./
+COPY . .
 
 # Install dependancies
 RUN yarn install
 
-# Run tests
-# RUN yarn test
+# Run tests (build fails if tests fail)
+RUN yarn test
 
 # Run in productionmode
 CMD ["yarn", "production"]
-
-# EXPOSE 3000
-EXPOSE 3000
--- src/test/DotenvParser.test.ts
+++ src/test/DotenvParser.test.ts
@@ -2,12 +2,12 @@ import DotenvParser from "../utils/DotenvParser";
 
 describe("Dotenv parser is created sucessfully", () => {
   test("Dotenvparser instanciated", () => {
-    const dotenvParser = new DotenvParser("test");
+    const dotenvParser = new DotenvParser();
     expect(dotenvParser).toBeTruthy();
   });
 
   test("Values successfully read from dot env file", () => {
-    const dotenvParser = new DotenvParser("test");
+    const dotenvParser = new DotenvParser();
     expect(dotenvParser.get("API_KEY")).toEqual("4p1_k3y");
   });
 });
